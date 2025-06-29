1001f11efdb828dc77d5d97f5ce09c556070fd92
Gabriel de Souza Miranda <gasmiranda07@gmail.com>
2025-06-29 10:21:03 -0300

    Referência API

--- API/ApiDaSprint04/obj/Debug/net8.0/ApiDaSprint04.AssemblyInfo.cs
+++ API/ApiDaSprint04/obj/Debug/net8.0/ApiDaSprint04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiDaSprint04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0e1fd8721f5372d2e2e0fc4f02bf615b99ed7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9bc3816abc9ed90bbb2770ee2fcaa32063047e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiDaSprint04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiDaSprint04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
